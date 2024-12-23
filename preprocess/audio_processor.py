2f12efab89922e81f441b8049a5bf1e731cf62ef
Lucas Mortensen <lucas37c@gmail.com>
2024-12-23 12:46:41 +0100

    tries to implement working audio processing functions

--- preprocess/audio_processor.py
+++ preprocess/audio_processor.py
@@ -1,14 +1,25 @@
 import librosa
+from librosa.effects import trim
 import numpy as np
+import os
 
 def load_audio(file_path, sr=22050):
-    audio, _ = librosa.load(file_path, sr=sr)
+    audio = librosa.load(file_path, sr=sr)
     return audio
 
 def clean_audio(audio):
-    # Apply noise reduction or normalization
-    return audio  # Placeholder
+    purified_audio = []
+    purified_audio[0] = audio[0]/np.max(np.abs(audio[0]))
+    purified_audio[0],purified_audio[2] = trim(audio)
+    return purified_audio
 
 def generate_spectrogram(audio, sr=22050):
     mel_spectrogram = librosa.feature.melspectrogram(audio, sr=sr, n_fft=1024, hop_length=256, n_mels=80)
     return librosa.power_to_db(mel_spectrogram)
+
+if __name__ == "__main__":
+    path = 'data/SamHarrisMedi.mp3'
+    audio = load_audio(path)
+    print(audio[0].shape,audio[1])
+    audio = clean_audio(audio)
+    print(audio[0].shape,audio[1])
